a52424afca4d02f7efb4920920ea67af392e062b
Your Name <ismazpek@gmail.com>
2023-10-18 04:45:11 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the characters from 'a' to 'z' using a while loop
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+    char b = 'a';
+    char c = 'z';
+
+    while (b <= c)
+    {
+        putchar(b);
+        b++;
+    }
+
+    return (0);
+}
+
