8c836b4d68c2ba0482166fa03a3549dd0259f47c
Your Name <ismazpek@gmail.com>
2023-10-19 01:07:32 -0700

    5-print_numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
-        int a;
-        for (a = 0; a < 10; a++)
-        {
-                printf("%d", a);
-        }
-	printf("\n");
+    int a;
+    for (a = 0; a < 10; a++)
+    {
+        printf("%d", a);
+    }
+    printf("\n");
 
-        return (0);
+    return (0);
 }
 
