29b1594e8e254b3044dbd582d76390a63dc88747
Your Name <ismazpek@gmail.com>
2023-10-19 02:04:43 -0700

    7-print_tebahpla

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,11 +7,14 @@
  */
 int main(void)
 {
-    char letter;
-    for (letter = 'z'; letter >= 'a'; letter--)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
-    return (0);
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
 }
+
