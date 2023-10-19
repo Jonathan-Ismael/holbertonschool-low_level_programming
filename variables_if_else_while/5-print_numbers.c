9629e875b4a24eceeb257ca9aa393b52b1557d7c
Your Name <ismazpek@gmail.com>
2023-10-19 01:09:00 -0700

    5-print_numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-    int a;
-    for (a = 0; a < 10; a++)
-    {
-        printf("%d", a);
-    }
-    printf("\n");
+	int a;
 
-    return (0);
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+
+	return (0);
 }
 
