e4741f31cd1ec0775003a6bafc8f144b5f8a3864
Your Name <ismazpek@gmail.com>
2023-10-22 02:58:56 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - main description
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	while (h != 24)
+	{
+		if (m == 60)
+		{
+			h++;
+			m = 0;
+		}
+		if (h == 24)
+			break;
+
+		if (h <= 9)
+			printf("0");
+		printf("%i:", h);
+
+		if (m <= 9)
+			printf("0");
+		printf("%i\n", m);
+		m++;
+
+	}
+}
