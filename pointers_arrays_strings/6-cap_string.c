29b63cc16b452afeafb27e13773b9b531a66cfb8
Your Name <ismazpek@gmail.com>
2023-10-29 07:46:33 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *cap_string - main description
+ * @str : pointers
+ * Return: Always 1 (True) or 0 (False)
+ */
+char *cap_string(char *str)
+{
+	char *tab = " \n\t,;.!?\"\'(){}";
+	int i = 0;
+	unsigned long int j = 0;
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= strlen(tab) ; j++)
+		{
+			if (str[i - 1] == tab[j])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
