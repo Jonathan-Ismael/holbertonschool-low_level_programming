03535e375a12b7f8e9ed9fb74ed8ac6766cd7487
Your Name <ismazpek@gmail.com>
2023-10-29 07:36:34 -0700

    9-

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - point
+ * @dest : pointers
+ * @src : variable
+ * Return: Always 1 (True) or 0 (False)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0 ; i <= len ; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
