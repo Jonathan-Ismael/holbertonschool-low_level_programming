68ef5396e6912ece9fcb0fd8ddc1bb1d39fab3db
Jonathan Ismael <ismazpek@gmail.com>
2023-11-19 09:21:46 -0800

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,8 +3,7 @@
 /**
  * sum_them_all - sums the parameters
  * @n: parameter
- * Return 0: if n == 0 or  sum of otherwise
- *
+ * Return: 0 if n == 0 or  sum of otherwise
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -13,12 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args;
+
 	va_start(args, n);
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	for (i = 0; i < n; i++)	
+
+	for (i = 0; i < n; i++)
 	{
 	sum += va_arg(args, int);
 	}
