24af2d025b35971a1ee4ce8d88a370478be38a98
Jonathan Ismael <ismazpek@gmail.com>
2023-12-20 01:45:33 -0800

    preprocessor/

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from
- * Return: 0
+ * main - function to print the name of the file
+ * Return: 0 on success
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_SUM_H
-#define FUNCTION_SUM_H
-
+#ifndef SUM_H
+#define SUM_H
 #define SUM(x, y) ((x) + (y))
-
-#endif
+#endif /* SUM */
